de3c1c1834a923a48c3f32f1614f27018b4db7b7
dipchandansingh1 <officialdipchandan@gmail.com>
2024-06-23 11:24:26 +0545

    cors

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const app=express();
 config({path:"./config/config.env"});
 
 
-app.use(cors)
 
-app.use(cors({
-    // origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL,process.env.DOCTORDASHBOARD_URL],~
-    methods:["GET","HEAD","PUT","PATCH","POST","DELETE"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL,process.env.DOCTORDASHBOARD_URL],
+//     methods:["GET","HEAD","PUT","PATCH","POST","DELETE"],
+//     credentials: true
+// }));
+app.use(cors)
 
 app.use(cookieParser())
 app.use(express.json())
